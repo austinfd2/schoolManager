a594ed7a9dde1effd253fa613d44d2f6559e8908
Austin Daelemans <austin.daelemans@nexius.com>
2017-04-06 14:40:04 -0500

    Updated student Class to be able to push student to db

--- studentClass.py
+++ studentClass.py
@@ -1,16 +1,29 @@
+import MySQLdb
+#open database connection
+db = MySQLdb.connect("localhost", "root", "Faulkner7", "db")
+
+#prepare a cursor object 
+cursor = db.cursor()
+
+
 class Student:
 	#base class for all students
-	total_students = 0
 
-	def __init__(self, first_name, last_name):
+	def __init__(self, first_name, last_name, id):
 		self.first_name = first_name
 		self.last_name = last_name
-		Student.total_students += 1
-		self.id = Student.total_students
+		self.id = id
 
 	def display_student(self):
 		print 'Id :' , self.id, ", Name :", self.first_name, self.last_name
 
-student1 = Student("Austin", "Daelemans")
+	def add_student_to_db(self):
+		sqlstring = """INSERT INTO STUDENTS (ID, FIRST, LAST) VALUES ({0},'{1}','{2}');"""
+		cursor.execute(sqlstring.format(student1.id,student1.first_name,student1.last_name))
+		db.commit()
+		db.close()
 
-student1.display_student()
+student1 = Student("Austin", "Daelemans", 1)
+student1.add_student_to_db()
+
+
