befc21809290ceca19a77c18bb062cea96505788
Austin Daelemans <austin.daelemans@nexius.com>
2017-04-06 14:53:35 -0500

    Improved System to assign next available ID

--- studentClass.py
+++ studentClass.py
@@ -1,5 +1,7 @@
 import MySQLdb
 #open database connection
+
+
 db = MySQLdb.connect("localhost", "root", "Faulkner7", "db")
 
 #prepare a cursor object 
@@ -8,22 +10,27 @@ cursor = db.cursor()
 
 class Student:
 	#base class for all students
-
-	def __init__(self, first_name, last_name, id):
+	student_id = cursor.execute("SELECT MAX(ID) FROM STUDENTS;")
+	print student_id
+	def __init__(self, first_name, last_name):
 		self.first_name = first_name
 		self.last_name = last_name
-		self.id = id
+		self.id = Student.student_id + 1
 
 	def display_student(self):
 		print 'Id :' , self.id, ", Name :", self.first_name, self.last_name
 
 	def add_student_to_db(self):
 		sqlstring = """INSERT INTO STUDENTS (ID, FIRST, LAST) VALUES ({0},'{1}','{2}');"""
-		cursor.execute(sqlstring.format(student1.id,student1.first_name,student1.last_name))
+		result = cursor.execute(sqlstring.format(student1.id,student1.first_name,student1.last_name))
+		if result:
+			print "Success"
+		else:
+			print "Error"
 		db.commit()
 		db.close()
 
-student1 = Student("Austin", "Daelemans", 1)
+student1 = Student("Austin", "Daelemans")
 student1.add_student_to_db()
 
 
