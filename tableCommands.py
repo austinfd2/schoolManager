fe9604f2e220a201f7de3bea69c0ff8f5db04db0
Austin Daelemans <austin.daelemans@nexius.com>
2017-04-07 14:01:02 -0500

    Created enroll and create class CLI calls

--- tableCommands.py
+++ tableCommands.py
@@ -1,5 +1,5 @@
 from studentClass import Student
-from teacherClass import Staff
+from teacherClass import Staff, Teacher
 import MySQLdb
 import sys
 
@@ -12,5 +12,13 @@ if arg[0] == "--createStudent":
 if arg[0] == "--createStaff":
 	staff = Staff(arg[1], arg[2])
 	staff.add_staff_to_db()
+
+if arg[0] == "--createClass":
+	teacher = Teacher(arg[1], arg[2])
+	teacher.create_class(arg[3])
+
+if arg[0] == "--enroll":
+	student = Student(arg[1], arg[2])
+	student.enroll()
 
 
